dfcb4f88444f704723b363b4bc9b39afbb2ee7f3
Josue0625 <jcampos@ufpso.edu.co>
2022-10-05 14:14:11 -0500

    Se creo el proyecto, descargando las dependencias

---
+++ src/main/java/com/primer_parcial_prueba/josue/PrimerParcialPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.primer_parcial_prueba.josue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerParcialPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerParcialPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primer_parcial_prueba/josue/PrimerParcialPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primer_parcial_prueba.josue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerParcialPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
